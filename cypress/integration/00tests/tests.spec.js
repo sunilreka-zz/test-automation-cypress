d0602b30128b5a0c406aa2523d90f3e2b743f78d
Sunil Reka <sunilreka@gmail.com>
2021-05-16 23:21:42 +0530

    home page - search for flights

--- cypress/integration/00tests/tests.spec.js
+++ cypress/integration/00tests/tests.spec.js
@@ -1,11 +1,34 @@
 /// <reference types="cypress" />
+import HomePage from '../PageObjects/HomePage';
 require('cypress-xpath');
+import cyConfig from './../../../cypress.json'
 
-describe('test class', () => {
-    it('test1', () => {
-        cy.visit('https://example.cypress.io/commands/actions')
-        cy.xpath('//input[@name="seg0_from"]').type('HYD');
-        // cy.type('\uE015');
-        // cy.type('\uE007');
+describe('Just Fly : Flight search', () => {
+    it('Search for a flight', () => {
+        cy.visit('https://www.justfly.com/', { timeout: cyConfig.defaultTimeOut });
+        const homepage = new HomePage();
+
+        //Enter From and To location
+        homepage.EnterLocations('HYD', 'CDG');
+
+        //Select From Date
+        homepage.SelectFromDate();
+
+        //Select To Date - DD
+        homepage.SelectToDate('30', 'June', '2021');
+
+        //Click on Search button
+        homepage.ClickOnSearch();
+    })
+
+    it('Dynamic Date', () => {
+        debugger;
+        var dt = new Date();
+        var dd = dt.getDate();
+        var monthname = dt.toLocaleString('default', { month: 'long' });
+        var yyyy = dt.getFullYear();
+
+        var calTitle = monthname + ' ' + yyyy;
+        var dt1 = new Date(2021, 8, 30);
     })
 })
